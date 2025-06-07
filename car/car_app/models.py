a7e5ebb6b17dd6767bdacfc7a70543be1fce340f
Dark Shadoow <kunwarbibek66632@gmail.com>
2025-06-07 20:56:03 +0545

    Validations In REST Framework || Field-Level , Validators & Object-Level

--- car/car_app/models.py
+++ car/car_app/models.py
@@ -7,6 +7,7 @@ class Carlist(models.Model):
     name = models.CharField(max_length=50, default='Unnamed')
     desc = models.CharField(max_length=100, default='No description')
     active = models.BooleanField(default=False)
+    
 
     def __str__(self):
         return self.name
--- car/car_app/views.py
+++ car/car_app/views.py
@@ -1,95 +1,42 @@
-# from django.shortcuts import render
-
-# # Create your views here.
-# from .models import Carlist
-# from django.http import JsonResponse
-
-# def car_list_view(request):
-#     car = Carlist.objects.all()
-#     data = {
-#         'cars': list(car.values()),
-#     }
-#     return JsonResponse(data)
-
-# from django.http import JsonResponse, HttpResponse  # Corrected: HttpResponse was misspelled and not imported
-# from .models import Carlist
-import json  # Required for json.dumps
-
-#2. View to list all cars
-# def car_list_view(request):
-#     cars = Carlist.objects.all()
-#     data = {
-#         'cars': list(cars.values())
-#     }
-#     data_json = json.dumps(data, indent=4)  # Optional: formatted JSON
-#     return HttpResponse(data_json, content_type='application/json')  # Corrected: HttpResponse spelling
-
-# # View to get a single car by primary key (pk)
-# def car_list_detail(request, pk):
-#     try:
-#         car = Carlist.objects.get(pk=pk)
-#         data = {
-#             'name': car.name,
-#             'desc': car.desc,
-#             'active': car.active,
-#         }
-#     except Carlist.DoesNotExist:
-#         return JsonResponse({'error': 'Car not found'}, status=404)
-
-#     return JsonResponse(data, json_dumps_params={'indent': 4})
-
-
-from django.http import JsonResponse, HttpResponse  
-from .models import Carlist
-from .api_file.serializer import CarSerializer 
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
-from rest_framework.response import Response
+from .models import Carlist
+from .api_file.serializer import CarSerializer
 
-
-
-
-@api_view(['GET','POST','DELETE'])  
+@api_view(['GET', 'POST'])  
 def car_list_view(request):
-    if request.method=='GET':
-        try:
-            cars = Carlist.objects.all()
-        except:
-            return Response({'Error':'car not found'},status=status.HTTP_400_BAD_REQUEST)
-
-        serializer = CarSerializer(cars, many=True)  # many=True because it's a queryset
+    if request.method == 'GET':
+        cars = Carlist.objects.all()
+        serializer = CarSerializer(cars, many=True)
         return Response(serializer.data)
-    if request.method=='POST':
-        serializer=CarSerializer(data=request.data)
+
+    elif request.method == 'POST':
+        serializer = CarSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
-        else:
-            return Response(serializer.errors)
-        
-    
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view()  
+
+@api_view(['GET', 'PUT', 'DELETE'])  
 def car_list_detail(request, pk):  
-    if request.method=='GET':
-        car = Carlist.objects.get(pk=pk)  
+    try:
+        car = Carlist.objects.get(pk=pk)
+    except Carlist.DoesNotExist:
+        return Response({'error': 'Car not found'}, status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
         serializer = CarSerializer(car)
         return Response(serializer.data)
-    if request.method=='PUT':
-        car = Carlist.objects.get(pk=pk) 
-        serializer=serializer(car, data=request.data)
+
+    elif request.method == 'PUT':
+        serializer = CarSerializer(car, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
-        else:
-            return Response(serializer.error,status=status.HTTP_400_BAD_REQUEST)
-    if request.method=='DELETE':
-        car = Carlist.objects.get(pk=pk) 
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
         car.delete()
         return Response({"message": "Car deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
-
-        
-
-
-
